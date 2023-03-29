567379df3aadaac69b18016a43d5e7b15b0c408e
kanlipy <127745314+kanlipy@users.noreply.github.com>
2023-03-29 17:18:53 +0530

    e

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,11 +18,12 @@ return new class extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('name');
+            $table->string('image')->nullable();
             $table->string('gender')->nullable();
             $table->string('about')->nullable();
             $table->string('fblink')->nullable();
             $table->string('twlink')->nullable();
-            $table->string('instalink')->nullable();
+            $table->string('instalink')->nullable();          
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
