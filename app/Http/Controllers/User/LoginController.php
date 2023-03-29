f2f73089ad2a1c644e777c02ebd7ece337634280
kanlipy <127745314+kanlipy@users.noreply.github.com>
2023-03-29 11:29:20 +0530

    a

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -11,9 +11,6 @@ class LoginController extends Controller
     {
         return view('login');
     }
-    public function loginmobile()
-    {
-        return view('frontend.loginmobile');
-    }
+   
     
 }
---
+++ storage/framework/views/57d4c67597966028a82a62d3f096e6be48bd6528.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=0, initial-scale=1.0">
+    <link rel="icon" href="<?php echo e(url('frontend/images/logo/logo.png')); ?>" type="images/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="<?php echo e(url('frontend/css/bootstrap_v4.3.1_min.css')); ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(url('frontend/css/owl.carousel.min.css')); ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(url('frontend/css/owl.theme.default.min.css')); ?>" type="text/css">
+    <link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all">
+    <link rel="stylesheet" href="<?php echo e(url('frontend/css/style.css')); ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(url('frontend/css/custom.css')); ?>" type="text/css">
+</head>
+<body>
+   
+<div class="nit-mobile-box">
+   <div class="nit-close-btn">
+        <a class="nit-close" href="<?php echo e(url('/')); ?>"><i class="fas fa-times"></i></a>
+    </div>
+    <div class="login-box">
+        <div class="box">
+            <i class="fas fa-user"></i>
+            <h3>Login</h3>
+        </div>
+        <form action="">
+            <input type="text" placeholder="Email Address">
+            <input type="password" placeholder="Password">
+            <input type="submit" class="btn" >
+            <p>Forgot Your Password?</p>
+        </form>
+    </div>
+</div>  
+   
+<script src="<?php echo e(url('frontend/js/jQuery_v3.4.1_min.js')); ?>"></script>
+<script src="<?php echo e(url('frontend/js/bootstrap_v4.3.1_min.js')); ?>"></script>
+<script src="<?php echo e(url('frontend/js/owl.carousel.min.js')); ?>"></script>
+<script src="<?php echo e(url('frontend/js/custom.js')); ?>"></script>
+
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\lara\feenu2803\resources\views/frontend/loginmobile.blade.php ENDPATH**/ ?>
