43d2c00ea1bd61c927a891da8a4c3cb5e82749b6
kanlipy <127745314+kanlipy@users.noreply.github.com>
2023-04-06 18:05:36 +0530

    d

--- resources/views/admin/contact.blade.php
+++ resources/views/admin/contact.blade.php
@@ -10,16 +10,17 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Enquiry Details</h1>
-      
-    </div>
+ 
 
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
       
         <div class="card-body">
             <div class="table-responsive">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Enquiry Details</h1>
+      
+    </div>
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
--- storage/framework/views/b53b20a5c167f5702e484dfac89ae6a0ca6a3ade.php
+++ storage/framework/views/b53b20a5c167f5702e484dfac89ae6a0ca6a3ade.php
@@ -15,14 +15,14 @@
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Reports</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Report Details</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
-                            <th>Comment</th>
+                            <th>Report</th>
                             <th>Game</th>
                             <th>User</th>
                         </tr>
